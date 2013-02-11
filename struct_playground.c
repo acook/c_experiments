947a1f3de21aa9114851f9bf8020bedd5af2acab
Anthony Cook <anthonymichaelcook@gmail.com>
2013-02-10 18:30:09 -0800

    Refactored algo out into its own function.

--- struct_playground.c
+++ struct_playground.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-typedef struct { char some_chr; char some_other_chr; } analyze_this;
+typedef struct { char some_chr; char some_other_chr; } SomeStruct;
 
 int main() {
-  unsigned char byte_from_struct;
   unsigned int struct_size;
   int *pointer_to_struct;
+  SomeStruct analyze_this;
 
   struct_size = sizeof(analyze_this);
   pointer_to_struct = &analyze_this;
 
   printf(" -- struct(analyze_this) size: %d \n", struct_size);
 
+  print_hex_bytes_from_sequence(pointer_to_struct, sizeof(analyze_this));
+}
+
+void print_hex_bytes_from_sequence(const char* c, unsigned int length) {
+  unsigned int byte_from_struct;
   int i;
-  for(i = 0; i < struct_size; i++) {
-    printf("%02X ", pointer_to_struct[i]);
+
+  for(i = 0; i < length; i++) {
+    byte_from_struct = c[i];
+    printf("%02X ", byte_from_struct);
   }
 
   printf("\n");
