a065d942e413fc713fd3788bfe6f6b39bea9430d
Anthony Cook <anthonymichaelcook@gmail.com>
2013-02-10 17:58:24 -0800

    Wrote out the rest of the algo... I think. Getting compile errors though.

--- struct_playground.c
+++ struct_playground.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-typedef struct { char* c; char b; } analyze_this;
+typedef struct { char some_chr; char some_other_chr; } analyze_this;
 
 int main() {
-  unsigned char c;
-  printf(" -- Struct.size: %d \n", sizeof(analyze_this));
+  unsigned char byte_from_struct;
+  unsigned int struct_size;
+  int *pointer_to_struct;
+
+  struct_size = sizeof(analyze_this);
+  pointer_to_struct = &analyze_this;
+
+  printf(" -- struct(analyze_this) size: %d \n", struct_size);
+
+  int i;
+  for(i = 0; i < struct_size; i++) {
+    printf("%02X ", pointer_to_struct[i]);
+  }
+
+  printf("\n");
 }
