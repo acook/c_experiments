e5746e04348ef9ba5a22af0de08b2ec87d82e386
Anthony Cook <anthonymichaelcook@gmail.com>
2013-02-10 18:47:36 -0800

    Added values to struct so we can identify them if we see them. Try to increment pointer.

--- struct_playground.c
+++ struct_playground.c
@@ -5,14 +5,14 @@ typedef struct { char some_chr; char some_other_chr; } SomeStruct;
 int main() {
   unsigned int struct_size;
   int *pointer_to_struct;
-  SomeStruct analyze_this;
+  SomeStruct analyze_this = {.some_chr = "a", .some_other_chr = "z"};
 
   struct_size = sizeof(analyze_this);
   pointer_to_struct = &analyze_this;
 
   printf(" -- struct(analyze_this) size: %d \n", struct_size);
 
-  print_hex_bytes_from_sequence(pointer_to_struct, sizeof(analyze_this));
+  print_hex_bytes_from_sequence(pointer_to_struct, struct_size);
 }
 
 void print_hex_bytes_from_sequence(const char* c, unsigned int length) {
@@ -20,7 +20,8 @@ void print_hex_bytes_from_sequence(const char* c, unsigned int length) {
   int i;
 
   for(i = 0; i < length; i++) {
-    byte_from_struct = c[i];
+    c++;
+    byte_from_struct = *c;
     printf("%02X ", byte_from_struct);
   }
 
